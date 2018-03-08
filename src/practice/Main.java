e80df3826eaf728e6109aa1a683ceb762cad98c9
Jason Zelmanovich <jasonzelm7@gmail.com>
2018-03-08 14:45:51 -0500

    Initial commit

---
+++ src/practice/Main.java
@@ -0,0 +1,10 @@
+
+package practice;
+
+class Main{
+
+    public static void main(String[] args){
+        Hamburger hamburger = new Hamburger("Basic" , "Sausage", 3.56, "White");
+        hamburger.itemizeHamburger();
+    }
+}
